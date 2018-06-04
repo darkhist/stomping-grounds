e380d0c336295a8dc15b95881d07000802ec3117
qmsalas <qmsalas@iastate.edu>
2018-06-03 21:32:24 -0500

    add bootstrap. setup init navbar

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Navigation from './navigation';
 
 const App = () => (
