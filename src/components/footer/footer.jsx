0abbd37dd165c2214484a575e855bee13253b79a
qmsalas <qmsalas@iastate.edu>
2018-10-22 13:13:29 -0500

    add footer content

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 import './footer.scss';
 
 const Footer = () => (
-  <div>
-    <div className="ftr">~cool footer goes here~</div>
-  </div>
+  <footer className="footer">
+    <p className="copywright"> &copy; Stomping Grounds Cafe </p>
+    <p className="address-line-one"> 303 Welch Avenue </p>
+    <p className="address-line-two"> Ames, Iowa 50014 </p>
+    <p className="phone"> 515-292-5258 </p>
+  </footer>
 );
 
 export default Footer;
